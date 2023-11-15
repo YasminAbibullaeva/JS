9326ea09af30dc67163eddca08111186db53b849
Yasmin <yasminabibullaeva@gmail.com>
2023-11-14 19:30:49 -0800

    java script

---
+++ java.js
@@ -0,0 +1,57 @@
+let a = 2
+let b = 3
+console.log(a*b);
+let perimetr = 2*(a+b)
+console.log(perimetr)
+let hello = "HEllo "
+let world = "World "
+let c = "!"
+console.log(hello +world+c)
+
+let number1 = Number(prompt("Введите число 1"))
+let number2 =  Number(prompt("Введите число 2"))
+let addition=(number1+number2)
+console.log (addition)
+
+let number3 =  Number(prompt("Введите число 1"))
+let number4 =  Number(prompt("Введите число 1"))
+let subraction=(number3-number4)
+console.log(subraction)
+
+let number5 =  Number(prompt("Введите число 1"))
+let number6 =  Number(prompt("Введите число 2"))
+let division=(number5/number6)
+console.log(division)
+
+let x =  Number(prompt("Введите число x"))
+let y =  Number(prompt("Введите число y"))
+
+let Perimetr = 2*(y+x)
+console.log(Perimetr)
+
+let d =  Number(prompt("Введите число d"))
+let e =  Number(prompt("Введите число e"))
+
+let square = (d*e)/2
+console.log(square)
+
+let figure1 = 7
+let figure2 = 7
+let figure3 = 4
+console.log((figure1+figure2+figure3))
+let Square = (figure1+figure2+figure3)/2
+console.log(Square)
+
+
+ 
+let planeName = prompt("Введите название самолёта ")
+let S = prompt("Введите расстояние ")
+let t = prompt("Введите время полёта ")
+let V = S/t
+console.log("Самолёт " + planeName + "летел " + t + "часов со скоростью " +V + " км.час ")
+
+let temp = prompt("Температура в гардусах по Цельсию ")
+let cloud = prompt("Ясно или облачно ")
+let wind = prompt("Скорость ветра в м/с ")
+console.log("Сегодня " + temp + "градусов по цельсию " + cloud + ".Скорость ветра " + wind + "м/с")
+let clock = promppt("Время?")
